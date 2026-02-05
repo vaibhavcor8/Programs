da49987a6d9edd2e2db997c6e6e183134c1829a4
Vaibhav Srivastava <vaibhavsriv@google.com>
2026-02-05 08:13:33 +0530

    first commit

---
+++ Generate_parenthesis.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        
+        def backtrack(current_string, open_count, close_count):
+           
+            if len(current_string) == 2 * n:
+                result.append(current_string)
+                return
+            
+            if open_count < n:
+                backtrack(current_string + "(", open_count + 1, close_count)
+            
+            if close_count < open_count:
+                backtrack(current_string + ")", open_count, close_count + 1)
+        
+        backtrack("", 0, 0)
+        return result
